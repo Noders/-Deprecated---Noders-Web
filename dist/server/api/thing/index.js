22e9f78cbb153676340d907e558e5ef6731c5233
fforres mac <felipe.torressepulveda@gmail.com>
2015-05-15 12:46:17 -0300

    dist folder

---
+++ dist/server/api/thing/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var express = require('express');
+var controller = require('./thing.controller');
+
+var router = express.Router();
+
+router.get('/', controller.index);
+router.get('/partners', controller.partners);
+router.get('/noders', controller.noders);
+router.get('/map', controller.map);
+
+module.exports = router;
